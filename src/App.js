ca0e2a53478e59ca78f99d7af546dfd771321c35
SeoYeongHoon <tjdudgns1996@naver.com>
2020-12-03 17:05:57 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import Board from './components/Board';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const boards = [
   {
@@ -31,27 +49,44 @@ const boards = [
 
 class App extends Component {
   render() {
+
+    const { classes } = this.props; // 위에서 정의한 스타일 적용시키기 위함
+    
     return (
-      <div>
-        {
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
           // 배열의 각 원소의 내용을 적용하기 위해 map이용
-          boards.map(c => { // 하나의 데이터를 c라고 가정
-            return (
-            <Board
-              key={c.id}  // map 사용하면 key 필요함
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-            );
-          })
-        }
-      </div>
+              boards.map(c => { // 하나의 데이터를 c라고 가정
+                return (
+                <Board
+                  key={c.id}  // map 사용하면 key 필요함
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+                );
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,38 +1,21 @@
 // 게시판 작성 칸
 
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow'; // 한 행
+import TableCell from '@material-ui/core/TableCell'; // 한 칸
+
 
 class Board extends React.Component {
     render() {
         return (
-            <div>
-                <BoardProfile id={this.props.id} image={this.props.image} name={this.props.name} />
-                <BoardInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-            </div>
-        )
-    }
-}
-
-class BoardProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile" />
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class BoardInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>{this.props.name}</h2>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
