9f8dcda2d7cfaa9cb1f8a286a83f4c3aa5ba53c0
SeoYeongHoon <tjdudgns1996@naver.com>
2020-12-03 16:25:09 +0900

    Create Board.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Board from './components/Board';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const boards = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '서영훈',
+    'birthday': '961205',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '서영우',
+    'birthday': '980917',
+    'gender': '남자',
+    'job': '소방관'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '김도은',
+    'birthday': '961216',
+    'gender': '남자',
+    'job': '프로그래머'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          // 배열의 각 원소의 내용을 적용하기 위해 map이용
+          boards.map(c => { // 하나의 데이터를 c라고 가정
+            return (
+            <Board
+              key={c.id}  // map 사용하면 key 필요함
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+            );
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,40 @@
+// 게시판 작성 칸
+
+import React from 'react';
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div>
+                <BoardProfile id={this.props.id} image={this.props.image} name={this.props.name} />
+                <BoardInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+            </div>
+        )
+    }
+}
+
+class BoardProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile" />
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class BoardInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+
+export default Board;
